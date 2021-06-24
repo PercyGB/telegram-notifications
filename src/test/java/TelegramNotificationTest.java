9f7e79152e14985fd219c8cf34c454083cf012e0
liana <kuznetsovalv@yandex.ru>
2021-06-24 13:07:27 +0400

    Initial version

---
+++ src/test/java/TelegramNotificationTest.java
@@ -0,0 +1,87 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TelegramNotificationTest {
+
+    @Test
+    void positiveTest1(){
+        assertTrue(true);
+    }
+
+    @Test
+    void positiveTest2(){
+        assertTrue(true);
+    }
+
+    @Test
+    void positiveTest3(){
+        assertTrue(true);
+    }
+
+    @Test
+    void positiveTest4(){
+        assertTrue(true);
+    }
+
+    @Test
+    void positiveTest5(){
+        assertTrue(true);
+    }
+
+    @Test
+    void negativeTest1(){
+        assertTrue(false);
+    }
+
+    @Test
+    void negativeTest2(){
+        assertTrue(false);
+    }
+
+    @Test
+    void negativeTest3(){
+        assertTrue(false);
+    }
+
+    @Test
+    void negativeTest4(){
+        assertTrue(false);
+    }
+
+    @Test
+    void negativeTest5(){
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void unusedTest1(){
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void unusedTest2(){
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void unusedTest3(){
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void unusedTest4(){
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void unusedTest5(){
+        assertTrue(true);
+    }
+}
